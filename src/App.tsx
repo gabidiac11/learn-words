19481c866509b3d614861c3896b8f56ebe4ce31f
Gabi <gabidiac11@gmail.com>
2024-02-24 01:08:39 +0200

    added navigation with login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Home from "./Pages/Home";
+import Login from "./Auth/Login";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useAuthInit } from "./Auth/authHooks";
+import { LoaderView } from "./Components/Loader";
 
 function App() {
+  const { user, isVerifying } = useAuthInit();
+  const isAuth = !!user;
+  if (isVerifying) {
+    return <LoaderView />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="main">
+        {user ? (
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route
+              path="*"
+              element={<DefaultRouteRedirection isAuth={isAuth} />}
+            />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="*"
+              element={<DefaultRouteRedirection isAuth={isAuth} />}
+            />
+          </Routes>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
+
+const DefaultRouteRedirection = (props: { isAuth: boolean }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    props.isAuth && navigate("/home", { replace: true });
+    !props.isAuth && navigate("/login", { replace: true });
+  }, [navigate, props.isAuth]);
+
+  return <LoaderView />;
+};
 
 export default App;
