b8794e226233a98f54642c79d7f5a21f91a1e8a6
Gabi <gabidiac11@gmail.com>
2024-02-24 01:46:05 +0200

    removed console log

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const json = process.env.REACT_APP_FIREBASE_CONFIG ?? "";
-console.log({json});
 
-const firebaseConfig = JSON.parse(json);
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG ?? "");
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
