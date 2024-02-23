60d356e0b7f66ba3ee11a04b90e4af561b189d88
Gabi <gabidiac11@gmail.com>
2024-02-24 01:39:38 +0200

    added stuff to debug

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+const json = process.env.REACT_APP_FIREBASE_CONFIG ?? "";
+console.log({json});
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG ?? "");
+const firebaseConfig = JSON.parse(json);
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
